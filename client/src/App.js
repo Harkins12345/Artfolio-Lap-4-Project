a6849e29b9e69d484baa66ca730298e67259478f
AlbertStoykov <albert.stoykov@gmail.com>
2022-06-04 15:13:00 +0100

    LandingPage
    Footer now responsive, created LandingPage with 2 carousells.

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { NotFoundPage } from "./pages";
 import { default as Layout } from "./layouts";
+import LandingPage from "./pages/LandingPage";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/" element={<LandingPage />}>
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
