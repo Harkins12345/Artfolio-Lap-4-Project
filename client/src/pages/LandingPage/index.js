629f43c95be4c1c6e6913f101c3d527ad3603173
AlbertStoykov <albert.stoykov@gmail.com>
2022-06-05 13:31:30 +0100

    LandingPage
    Buttons and sections

--- client/src/pages/LandingPage/index.js
+++ client/src/pages/LandingPage/index.js
@@ -6,11 +6,26 @@ const LandingPage = () => {
   return (
     <>
       <Layout />
+      <section className="sign-in">
+        <div className="container">
+          <figure className="gallery-item">
+            <img src="" alt="Main image" />
+          </figure>
+          <h3>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend
+            viverra dictumst posuere aliquet sem nullam in diam.
+          </h3>
+          <button class="btn btn-primary">Sign In</button>
+        </div>
+        <p>Haven't got an account? Create one now!</p>
+      </section>
 
       {/* GIG GALLERY */}
 
-      <section className="gallery">
+      <section className="gallery gig-gallery">
         <div className="container">
+          <button class="btn btn-primary">See All Gigs</button>
+
           <ul className="gallery-list has-scrollbar">
             <li>
               <figure className="gallery-item">
@@ -44,6 +59,7 @@ const LandingPage = () => {
       <section className="gallery">
         <div className="container">
           <ul className="gallery-list has-scrollbar">
+            <button class="btn btn-primary">See All Artists</button>
             <li>
               <figure className="gallery-item">
                 <img src="" alt="Artist image" />
