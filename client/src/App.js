6597ee30e3a9a63f5b036a625fb95560600c139a
jianli1028 <96356376+jianli1028@users.noreply.github.com>
2022-06-06 17:51:54 +0100

    all user page initialization

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { NotFoundPage } from "./pages";
 import { default as Layout } from "./layouts";
 import LandingPage from "./pages/LandingPage";
+import AllArtistsPage from "./pages/AllArtistsPage";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Route path="/" element={<LandingPage />}>
         <Route path="*" element={<NotFoundPage />} />
       </Route>
+      <Route path="/artists" element={<AllArtistsPage/>}/>
     </Routes>
   );
 };
