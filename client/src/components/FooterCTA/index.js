0fa4b3db654706020b051c530a1b96e454db0a2e
Prishal <prishalmakwana@gmail.com>
2022-06-07 00:25:01 +0100

    Footer cta for mobile started

--- client/src/components/FooterCTA/index.js
+++ client/src/components/FooterCTA/index.js
@@ -2,12 +2,16 @@ import React from "react";
 
 const FooterCTA = () => {
   return (
-    <section id="FooterCTA">
-      <div className="centered">
-        <h3 className="ready">Ready to get started?</h3>
-        <button className="btn btn-primary FooterCTA-btn">Sign In</button>
+    <section id="footer-cta">
+      <div className="container-xl py-4 footer-container">
+        <h3 id="footer-cta-title">Ready to get started?</h3>
+        <button className="btn sign-in-btn">Sign In</button>
+
+        <p id="create-account">
+          Haven't got an account?{" "}
+          <span className="createAccountNow">Create one now!</span>
+        </p>
       </div>
-      <p id="createAccount">Haven't got an account? Create one now!</p>
     </section>
   );
 };
