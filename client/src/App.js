e577a81d85c894fb1d6b11c5d874171591ea4651
Yusra <yusra-tahir@hotmail.co.uk>
2022-06-06 16:55:43 +0100

    sign-in/up and register page
    set the page up but still needs editing

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { NotFoundPage } from "./pages";
+import { NotFoundPage, AuthPage } from "./pages";
 import { default as Layout } from "./layouts";
 import LandingPage from "./pages/LandingPage";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />}>
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
+      <Route path="/" element={<LandingPage />}/>
+      <Route path="/sign-in" element={<AuthPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 };
