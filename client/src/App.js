9d99da663092c0e2f972d55987f3630ce2a3e85c
AlbertStoykov <albert.stoykov@gmail.com>
2022-06-06 18:24:39 +0100

    Layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { NotFoundPage, AuthPage } from "./pages";
-import { default as Layout } from "./layouts";
+import Layout from "./layouts";
 import LandingPage from "./pages/LandingPage";
 import Footer from "./layouts/Footer";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />}/>
-      <Route path="/sign-in" element={<AuthPage />} />
-      <Route path="*" element={<NotFoundPage />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/sign-in" element={<AuthPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
     </Routes>
   );
 };
