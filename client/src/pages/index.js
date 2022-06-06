1f7890b2b398b2dc2c979c102a7773ac5c7005d0
jianli1028 <96356376+jianli1028@users.noreply.github.com>
2022-06-06 18:38:54 +0100

    added export

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,4 +2,4 @@ export { default as NotFoundPage } from "./NotFoundPage";
 export { default as AuthPage } from "./AuthPage";
 export { default as LandingPage } from "./LandingPage";
 export { default as ArtistPortfolioPage } from "./ArtistPortfolioPage";
-
+export { default as AllArtistsPage } from "./AllArtistsPage"
