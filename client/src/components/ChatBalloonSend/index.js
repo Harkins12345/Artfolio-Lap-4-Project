469207563d621ab482de9ffe68c2875fef7ca1d8
jianli1028 <96356376+jianli1028@users.noreply.github.com>
2022-06-08 22:59:56 +0100

    renamed

---
+++ client/src/components/ChatBalloonSend/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// this is the chat balloon of sent messages
+
+const ChatBalloonSend = () => {
+    
+    return (
+        <></>
+    )
+}
+
+export default ChatBalloonSend;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -5,4 +5,4 @@ export { default as ArtistMedia } from "./ArtistMedia";
 export { default as ArtistCard } from "./ArtistCard";
 export { default as AcceptedRequest } from "./AcceptedRequest";
 export { default as ChatModal } from "./ChatModal";
-export { default as ChatBalloon } from "./ChatBalloon";
+export { default as ChatBalloonSend } from "./ChatBalloonSend";
