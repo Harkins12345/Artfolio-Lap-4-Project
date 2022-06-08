295f4262456f980b374e554f7a482e232496ce6b
Harkins12345 <49042556+Harkins12345@users.noreply.github.com>
2022-06-08 18:15:11 +0100

    Working on audio functionality

--- client/src/components/ArtistAudio/index.js
+++ client/src/components/ArtistAudio/index.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 const ArtistAudio = ({ media }) => {
+
+  const [paused, changePaused] = useState();
+  const [song, setSong] = useState();
+
+  function togglePause(e){
+    if (paused){
+      changePaused(false)
+    } else {
+      changePaused(true)
+    }
+    paused ? changePaused(false) : changePaused(true)
+  }
+
   return (
     <div className="media-audio" data-testid="media-audio">
       <div className="row ">
         <div className="col-2">
           <div className="play-btn">
-            <i className="bi bi-play-circle-fill"></i>
+            <i className={paused ? "bi bi-play-circle-fill" : "bi bi-pause-circle-fill"} onClick={togglePause}></i>
           </div>
         </div>
         <div className="col-10 audio-name" data-testid="audio-name">
