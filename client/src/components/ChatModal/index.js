a67432510de94d1fddc36b3d78a726d4c17c63c0
jianli1028 <96356376+jianli1028@users.noreply.github.com>
2022-06-08 15:46:46 +0100

    added base for the modal component

---
+++ client/src/components/ChatModal/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ChatModal = () => {
+
+
+    return (
+        <>
+            {showModal? (
+                <div className="chat-modal-box ">
+                    
+                </div>
+            ) : null}
+        </>
+    )
+}
+
+export default ChatModal;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,3 +3,4 @@ export { default as ArtistAudio } from "./ArtistAudio";
 export { default as ArtistReview } from "./ArtistReview";
 export { default as ArtistMedia } from "./ArtistMedia";
 export { default as ArtistCard } from "./ArtistCard";
+export { default as ChatModal } from "./ChatModal"
