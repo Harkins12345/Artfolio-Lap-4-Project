c84cab5c73505f423ddcf0808954701442403bab
Yusra <yusra-tahir@hotmail.co.uk>
2022-06-09 11:48:31 +0100

    updating readme.md and navigation to dashboard once signed in

--- client/src/pages/LandingPage/index.js
+++ client/src/pages/LandingPage/index.js
@@ -25,7 +25,7 @@ const LandingPage = () => {
       <section className="hero-section" data-testid="hero-section">
         <div className="container-xl hero-container" data-testid="hero-section">
           <img
-            src="https://s2.r29static.com/bin/public/be6/0,0,2000,1050/x,80/1501654/image.jpg"
+            src="https://images.pexels.com/photos/8043841/pexels-photo-8043841.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             alt=""
             className="hero-image my-3"
           />
